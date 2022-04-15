2177ff30fde3fefb0a918906c0b257440617c7af
s-BWU <s-BWU@487M089646>
2022-04-15 07:51:48 -0700

    fixed groups backend

--- Image_Guesser/Hubs/ChatHub.cs
+++ Image_Guesser/Hubs/ChatHub.cs
@@ -12,5 +12,18 @@ namespace Image_Guesser.Hubs
         {   
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
     }
 }
