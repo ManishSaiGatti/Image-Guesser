4291abab7dd7006bd0ebe925173ea295630596a9
s-MSUBOTIC <s-MSUBOTIC@lwsd.org>
2022-04-22 11:45:35 -0700

    working on adding in storage for each user

--- Image_Guesser/Hubs/ChatHub.cs
+++ Image_Guesser/Hubs/ChatHub.cs
@@ -8,22 +8,39 @@ namespace Image_Guesser.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message, string groupName)
-        {   
-            await Clients.Group(groupName).SendAsync("ReceiveMessage", user, message);
+        
+        private Dictionary<String, String> userStorage;
+        public ChatHub()
+        {
+            userStorage = new Dictionary<string, string>();
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            if (userStorage.ContainsKey(Context.ConnectionId))
+            {
+                Console.WriteLine("sending message rn");
+                await Clients.Group(userStorage.GetValueOrDefault(Context.ConnectionId)).SendAsync("ReceiveMessage", user, message);
+            }
+            
         }
         public async Task AddToGroup(string groupName)
         {
-            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-
-            await Clients.Group(groupName).SendAsync("ReceiveMessage", Context.ConnectionId, $"{Context.ConnectionId} has joined the group {groupName}.");
+            if (!userStorage.ContainsKey(Context.ConnectionId))
+            {
+                userStorage.Add(Context.ConnectionId, groupName);
+                await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+                Console.WriteLine(userStorage.GetValueOrDefault(Context.ConnectionId));
+                await Clients.Group(groupName).SendAsync("ReceiveMessage", Context.ConnectionId, $"{Context.ConnectionId} has joined the group {groupName}.");
+            }
+            
         }
 
-        public async Task RemoveFromGroup(string groupName)
+        public async Task RemoveFromGroup()
         {
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, userStorage.GetValueOrDefault(Context.ConnectionId));
+            await Clients.Group(userStorage.GetValueOrDefault(Context.ConnectionId)).SendAsync("Send", $"{Context.ConnectionId} has left the group {userStorage.GetValueOrDefault(Context.ConnectionId)}.");
+            userStorage.Remove(Context.ConnectionId);
 
-            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
         }
     }
 }
