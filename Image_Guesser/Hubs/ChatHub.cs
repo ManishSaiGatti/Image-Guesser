a8ae44a9f914837f6a0e00dfd4351c7c13f5bb47
s-BWU <s-BWU@487M089646>
2022-04-29 07:41:29 -0700

    working groups backend

--- Image_Guesser/Hubs/ChatHub.cs
+++ Image_Guesser/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,23 +7,44 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Image_Guesser.Hubs
 {
+    public class GroupSingleton
+    {
+        public static ArrayList groups = new ArrayList();
+ 
+    }
+
     public class ChatHub : Hub
     {
+
         public async Task SendMessage(string user, string message, string groupName)
         {   
             await Clients.Group(groupName).SendAsync("ReceiveMessage", user, message);
         }
-        public async Task AddToGroup(string groupName)
+        public async Task CreateGroup(string groupName)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-
+            GroupSingleton.groups.Add(groupName);
             await Clients.Group(groupName).SendAsync("ReceiveMessage", Context.ConnectionId, $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+        public async Task<bool> JoinGroup(string groupName)
+        {
+            foreach(string group in GroupSingleton.groups)
+            {
+                if(group.Equals(groupName))
+                {
+                    await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+                    // add user to group as well
+                    await Clients.Group(groupName).SendAsync("ReceiveMessage", Context.ConnectionId, $"{Context.ConnectionId} has joined the group {groupName}.");
+                    return true;
+                }
+            }
+                return false;
         }
 
         public async Task RemoveFromGroup(string groupName)
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
-
+            
             await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
         }
     }
